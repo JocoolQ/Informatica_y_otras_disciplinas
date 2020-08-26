0adfdba9343cf2422e1f40d857c4744ccb0b7e5b
Jonathan Steven Capera Quintana <jscaperaq@correo.udistrital.edu.co>
2020-08-25 23:44:07 -0500

    Update webvowl.app.js

--- js/webvowl.app.js
+++ js/webvowl.app.js
@@ -8223,7 +8223,7 @@ webvowl.app =
 	  var showLoadingDetails = false;
 	  var visibilityStatus = true;
 	  
-	  var DEFAULT_JSON_NAME = "infprmatica"; // This file is loaded by default
+	  var DEFAULT_JSON_NAME = "informatica"; // This file is loaded by default
 	  var conversion_sessionId;
 	  
 	  /** variable defs **/
